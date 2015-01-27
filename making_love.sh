21e0cd77bce0fd45eea30d9b52bf0d758bac9e0a
toxinu <toxinu@gmail.com>
2015-01-27 22:42:57 +0100

    Update making_love.sh

--- making_love.sh
+++ making_love.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
 NAME="love-builder"
-LOVE_DIR="/tmp/love"
+LOVE_DIR="/usr/src/love"
+LOVE_VERSION="0.9.0"
+BOOTSTRAP_DIR="$LOVE_DIR/builds"
+LOVE_URL="https://bitbucket.org/rude/love/downloads/love-$LOVE_VERSION-linux-src.tar.gz"
 
-# sudo lxc-create -t download -n $NAME -- -d ubuntu -r trusty -a amd64
-# sudo lxc-start -d -n $NAME
-# sudo lxc-attach -n $NAME -- apt-get update
-# sudo lxc-attach -n $NAME -- apt-get -y upgrade
-# sudo lxc-attach -n $NAME -- apt-get -y install mercurial
-# sudo lxc-attach -n $NAME -- apt-get install -y libsdl2-dev libopenal-dev libdevil-dev
-# sudo lxc-attach -n $NAME -- apt-get install -y libmodplug-dev libvorbis-dev libphysfs-dev
-# sudo lxc-attach -n $NAME -- apt-get install -y libluajit-5.1-dev libmpg123-dev
-# sudo lxc-attach -n $NAME -- apt-get install -y build-essential autoconf libtool libfreetype6-dev
-sudo lxc-attach -n $NAME -- bash -c "hg clone https://socketubs@bitbucket.org/rude/love $LOVE_DIR"
-sudo lxc-attach -n $NAME -- bash -c "mkdir $LOVE_DIR/builds"
-sudo lxc-attach -n $NAME -- bash -c "cd $LOVE_DIR && platform/unix/automagic -d"
-sudo lxc-attach -n $NAME -- bash -c "cd $LOVE_DIR && ./configure --prefix=$LOVE_DIR/builds"
-sudo lxc-attach -n $NAME -- bash -c "cd $LOVE_DIR && make"
+sudo lxc-create -t download -n $NAME -- -d ubuntu -r trusty -a amd64
+sudo lxc-start -d -n $NAME
 
 ARCH="i386"
 BITS="32"
@@ -26,20 +17,51 @@ then
     BITS="64"
 fi
 
-echo "Building bootstrap in $LOVE_DIR/builds/bootstrap/{bin,lib}..."
-sudo lxc-attach -n $NAME -- bash -c "mkdir $LOVE_DIR/builds/bootstrap/{bin,lib} -p"
-sudo lxc-attach -n $NAME -- bash -c "cp $LOVE_DIR/builds/lib/liblove.so.0 $LOVE_DIR/builds/bootstrap/lib"
-sudo lxc-attach -n $NAME -- bash -c "cp $LOVE_DIR/builds/bin/love $LOVE_DIR/builds/bootstrap/bin/love$BITS"
+sudo lxc-attach -n $NAME -- apt-get update
+sudo lxc-attach -n $NAME -- apt-get -y upgrade
+sudo lxc-attach -n $NAME -- apt-get install -y wget
+sudo lxc-attach -n $NAME -- apt-get install -y libsdl2-dev libopenal-dev libdevil-dev
+sudo lxc-attach -n $NAME -- apt-get install -y libmodplug-dev libvorbis-dev libphysfs-dev
+sudo lxc-attach -n $NAME -- apt-get install -y libluajit-5.1-dev libmpg123-dev
+sudo lxc-attach -n $NAME -- apt-get install -y build-essential autoconf libtool libfreetype6-dev
+sudo lxc-attach -n $NAME -- bash -c "if [ ! -d $LOVE_DIR ]; then mkdir -p $LOVE_DIR ;fi"
+sudo lxc-attach -n $NAME -- bash -c "cd $LOVE_DIR && if [ ! -f love-$LOVE_VERSION-linux-src.tar.gz ]; then wget $LOVE_URL ;fi"
+sudo lxc-attach -n $NAME -- bash -c "cd $LOVE_DIR && if [ ! -d $LOVE_DIR/love-$LOVE_VERSION ]; then tar xvf love-$LOVE_VERSION-linux-src.tar.gz ;fi"
+sudo lxc-attach -n $NAME -- bash -c "if [ ! -d $LOVE_DIR/builds ]; then mkdir $LOVE_DIR/builds ;fi"
 
+LOVE_DIR="$LOVE_DIR/love-$LOVE_VERSION"
+BOOTSTRAP_DIR="$BOOTSTRAP_DIR/love-$LOVE_VERSION-$ARCH"
+
+sudo lxc-attach -n $NAME -- bash -c "cd $LOVE_DIR && ./configure --prefix=$BOOTSTRAP_DIR"
+sudo lxc-attach -n $NAME -- bash -c "cd $LOVE_DIR && make"
+sudo lxc-attach -n $NAME -- bash -c "cd $LOVE_DIR && make install"
+sudo lxc-attach -n $NAME -- bash -c "if [ -d $BOOTSTRAP_DIR/share ]; then rm -r $BOOTSTRAP_DIR/share ;fi"
+
+echo "Building bootstrap in $BOOTSTRAP_DIR/{bin,lib}..."
+sudo lxc-attach -n $NAME -- bash -c "if [ ! -f $BOOTSTRAP_DIR/bin/love$BITS ]; then mv $BOOTSTRAP_DIR/bin/love $BOOTSTRAP_DIR/bin/love$BITS ;fi"
+sudo lxc-attach -n $NAME -- bash -c "if [ ! -d $BOOTSTRAP_DIR/lib/$ARCH ]; then mkdir $BOOTSTRAP_DIR/lib/$ARCH ;fi"
 
 LIBS="libIL.so libSDL2-2.0.so libXss.so libjpeg.so liblcms.so libluajit-5.1.so libmng.so libmodplug.so libmpg123.so libopenal.so libphysfs.so libpng12.so libtiff.so libvorbisfile.so"
-
-for lib in $(sudo lxc-attach -n $NAME -- bash -c "ldd $LOVE_DIR/builds/bin/love | awk '{ print $3 }' | sed  '/^$/d'"); do
-	for _lib in $LIBS; do
-		if [[ $lib == *$_lib* ]]; then
-			echo "Copying $lib ..."
-			sudo lxc-attach -n $NAME -- bash -c "cp $lib $LOVE_DIR/builds/bootstrap/lib"
-		fi
-	done
+for lib in $(sudo lxc-attach -n $NAME -- bash -c "ldd $BOOTSTRAP_DIR/bin/love$BITS"); do
+  for _lib in $LIBS; do
+    if [[ ${lib:0:1} == "/" ]]; then
+      if [[ $(basename $lib) == *$_lib* ]]; then
+        echo "Copying" $(basename $lib) "($lib)..."
+		sudo lxc-attach -n $NAME -- bash -c "cp $lib $BOOTSTRAP_DIR/lib/$ARCH"
+      fi
+    fi
+  done
 done
+
+sudo lxc-attach -n $NAME -- bash -c "mv $BOOTSTRAP_DIR/lib/liblove.* $BOOTSTRAP_DIR/lib/$ARCH"
+
+sudo lxc-attach -n $NAME -- bash -c "cd $BOOTSTRAP_DIR/.. && tar cvpzf $BOOTSTRAP_DIR/../love-$LOVE_VERSION-$ARCH-binaries.tar.gz $(basename $BOOTSTRAP_DIR)"
+sudo lxc-attach -n $NAME -- bash -c "cat $BOOTSTRAP_DIR/../love-$LOVE_VERSION-$ARCH-binaries.tar.gz" > src/love-$LOVE_VERSION-$ARCH.tar.gz
+
+cd src
+tar xvf love-$LOVE_VERSION-$ARCH.tar.gz
+mv love-$LOVE_VERSION-$ARCH love-$LOVE_VERSION
+rm love-$LOVE_VERSION-$ARCH.tar.gz
+cd -
+
 echo "Done."
